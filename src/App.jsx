8c5bc0c91953a6e21df82f249f48d93c747aa3e8
icodenextweb <icodenextweb@gmail.com>
2026-08-12 22:03:15 +0530

    Setup dashboard folder structure and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './assets/css/App.css'
+import Home from './pages/Home'
 
 function App() {
   return (
-   <><h1 className='p-4 w-full bg-blue-800 text-white'>Welcome to React!</h1></>
+   <>
+   <h1 className='p-4 w-full bg-blue-800 text-white'>Welcome to React!</h1>
+   <Home/>
+   </>
   )
 }
 
